13933f91cc11457d69d233bdc08377756159535f
masus65 <46897481+masus65@users.noreply.github.com>
2019-11-12 20:52:55 +0100

    save & load bin file
    session07

--- Session07Indi/Session07Indi/Source.cpp
+++ Session07Indi/Session07Indi/Source.cpp
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include<vector>
 #include "Osoba.h"
+#include <string>
 #define VELIKOST_POLE 3
 
 
@@ -23,36 +24,53 @@ void vypis(Osoba *osoby, int velikost) {
 
 //Ve funkci uloz() vytvoøte statické pole objektù Osoba(s alespoò 3 objekty) a zapište jej do souboru.
 void uloz() {
-	Osoba poleOsob[VELIKOST_POLE];
+	/*Osoba poleOsob[VELIKOST_POLE];
+	Osoba O1{ "Paja","Novakova",Adresa { "jan","Praha",45542 },Datum { 1,2,2002 } };
+	Osoba O2{ "Karel","Stary",Adresa { "Stara","Lutine",46122 },Datum { 3,4,2001 } };
+	Osoba O3{ "Karla","Buchtova",Adresa { "Mlada","Ostrava",12130 },Datum { 4,5,2000 } };
+	poleOsob[0] = O1;
+	poleOsob[1] = O2;
+	poleOsob[2] = O3;*/
+	
+	Osoba* poleOsob = new Osoba[VELIKOST_POLE];
 	Osoba O1{ "Paja","Novakova",Adresa { "jan","Praha",45542 },Datum { 1,2,2002 } };
 	Osoba O2{ "Karel","Stary",Adresa { "Stara","Lutine",46122 },Datum { 3,4,2001 } };
 	Osoba O3{ "Karla","Buchtova",Adresa { "Mlada","Ostrava",12130 },Datum { 4,5,2000 } };
 	poleOsob[0] = O1;
 	poleOsob[1] = O2;
 	poleOsob[2] = O3;
+	ofstream out{};
+	out.open("session07.dat", ios_base::binary);
 
-	std::streampos size;
-	char* memblock;
-	std::ifstream myFile("Session07.bin", std::ios::in | std::ios::binary | std::ios::ate);
-	if (myFile.is_open)
-	{
-		size = myFile.tellg();
-		memblock = new char[size];
-		myFile.seekg(0, std::ios::beg);
-		myFile.read(memblock, size);
-		myFile.close();
-		std::cout << "The entire thing is in memory";
-		delete[] memblock;
-	}
-	else {
-		std::cout << "Error, unable to open file";
+	if (out.is_open()) {
+		for (int i = 0; i < VELIKOST_POLE; i++)
+		{
+			out.write((char*)&poleOsob[i], sizeof(Osoba));
+		}
 	}
 
 }
 
 //Ve funkci nacti() naètìte osoby ze souboru do pole a vypište je na obrazovku.Pøi naèítání pole dynamicky alokujte, vhodným zpùsobem zjistìte poèet záznamù v souboru.
 void nacti() {
-	
+	ifstream in{};
+	in.open("session07.dat", ios_base::binary);
+	string s{};
+	//int count = 0;
+	Osoba* osoby = new Osoba[3];
+	if (in.is_open()) {
+		/*while (!in.eof()) {
+			getline(in,s);
+			count++;
+		}*/
+		for (int i = 0; i < 3; i++)
+		{
+			in.read((char*)&osoby[i], sizeof(Osoba));
+		}
+		in.close();
+	}
+	vypis(osoby, VELIKOST_POLE);
+	delete[] osoby;
 
 }
 
